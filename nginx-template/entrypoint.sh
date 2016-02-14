85b4495055ec4c307cd78b2c4409f964634260a1
Stefan Lapers <stefan@lapers.be>
2016-02-14 13:51:04 +0100

    add nginx template

---
+++ nginx-template/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$TEMPLATES" ]
+then
+  echo "No templates specified in env to parse"
+fi
+
+for _TEMPLATE in $TEMPLATES
+do
+  echo "processing template: ${_TEMPLATE}"
+  /usr/bin/perl -p -i -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg' ${_TEMPLATE}
+  echo "---------------------------------"
+  cat ${_TEMPLATE}
+done
+
+
+echo "Starting nginx"
+echo "--------------"
+/usr/sbin/nginx -g "daemon off;"
